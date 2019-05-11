09af3a077b0b09c4f0bceb9a66951be9e3bb134c
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-05-11 19:12:36 +0800

    site: footer | update 9

--- assets/js/footer-donasi.js
+++ assets/js/footer-donasi.js
@@ -2,13 +2,13 @@ function openPlatform(evt, platform) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = "block";
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(platform).style.display = "block";
+  document.getElementById(platform).style.display = "none";
   evt.currentTarget.className += " active";
 }
 
