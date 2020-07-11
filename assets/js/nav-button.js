4af42224ca29a959d6d334258811a876f19cc2d3
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-07-11 11:16:12 +0800

    Fix hamburger menu not show on mobile

--- assets/js/nav-button.js
+++
@@ -1,9 +0,0 @@
-var toggle = false;
-function navBtn() {
-    if (toggle === true) {
-        document.getElementById('nav-btn').src  = '/assets/img/logo/logo_menu.svg';
-    } else {
-        document.getElementById('nav-btn').src  = '/assets/img/logo/logo_close.svg';
-    }
-    toggle = !toggle;
-}
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-    $('.menu').click(function() {
-        $('ul').toggleClass('active');
-    })
-})
+var toggle = false;
+var menu = document.getElementById("menu-ul");
+function navBtn() {
+  if (toggle === true) {
+    document.getElementById('nav-btn').src  = '/assets/img/logo/logo_menu.svg';
+  } else {
+    document.getElementById('nav-btn').src  = '/assets/img/logo/logo_close.svg';
+  }
+  toggle = !toggle;
+
+  menu.classList.toggle("active")
+}
