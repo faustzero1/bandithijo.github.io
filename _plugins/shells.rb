e04a957744c84c17b854c038f17eebe5af946ad9
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-01-04 22:10:27 +0800

    Refactoring shells

--- _plugins/shells.rb
+++ _plugins/shells.rb
@@ -1,19 +1,9 @@
 module Jekyll
   class ShellRoot < Liquid::Block
     def render(context)
-      command = super.gsub(/\s+/, ' ')
-      text  = '<pre>'
-      text += "#<b>#{command}</b>"
-      text += '</pre>'
-      text
-    end
-  end
-
-  class ShellRoots < Liquid::Block
-    def render(context)
       commands = super.split("\n")
       text  = '<pre>'
-      text += "#{commands[1..].map { |i| "# <b>#{i}</b>\n" }.join}"
+      text += commands[1..].map { |i| "# <b>#{i}</b>\n" }.join.to_s
       text += '</pre>'
       text
     end
@@ -21,19 +11,9 @@ module Jekyll
 
   class ShellUser < Liquid::Block
     def render(context)
-      command = super.gsub(/\s+/, ' ')
-      text  = '<pre>'
-      text += "$<b>#{command}</b>"
-      text += '</pre>'
-      text
-    end
-  end
-
-  class ShellUsers < Liquid::Block
-    def render(context)
       commands = super.split("\n")
       text  = '<pre>'
-      text += "#{commands[1..].map { |i| "$ <b>#{i}</b>\n" }.join}"
+      text += commands[1..].map { |i| "$ <b>#{i}</b>\n" }.join.to_s
       text += '</pre>'
       text
     end
@@ -41,6 +21,4 @@ module Jekyll
 end
 
 Liquid::Template.register_tag('shell_root',  Jekyll::ShellRoot)
-Liquid::Template.register_tag('shell_roots', Jekyll::ShellRoots)
 Liquid::Template.register_tag('shell_user',  Jekyll::ShellUser)
-Liquid::Template.register_tag('shell_users', Jekyll::ShellUsers)
