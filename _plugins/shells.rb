ef011b1ee4285746c915b52c3c4583ae50d88dde
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-04-08 14:59:29 +0800

    Change shell_cmd to shell_term

--- _plugins/shells.rb
+++ _plugins/shells.rb
@@ -26,10 +26,10 @@ module Jekyll
   # SHELL PROMPT WITH CUSTOM PARAMETER
   # You can add custom prompt & color. And able to use with multiline.
   # How to use?
-  # {% shell_cmd <prompt_symbol> | <prompt_color:#000000> %}
+  # {% shell_term <prompt_symbol> | <prompt_color:#000000> %}
   # content...
-  # {% endshell_cmd %}
-  class ShellCommand < Liquid::Block
+  # {% endshell_term %}
+  class ShellTerm < Liquid::Block
     def initialize(tag_name, input, tokens)
       super
       @input = input
@@ -44,7 +44,39 @@ module Jekyll
       output  = '<pre>'
       output += commands[1..].map do |i|
         "<span class='cmd' #{"style='color:#{prompt_color};'" unless prompt_color.nil?}>" \
-          "#{prompt_symbol.nil? ? '$' : prompt_symbol} </span><b>#{i.rstrip}</b><br>"
+        "#{prompt_symbol.nil? ? '$' : prompt_symbol} </span><b>#{i.rstrip}</b><br>"
+      end.join.to_s
+      output += '</pre>'
+      output
+    end
+
+    def split_params(params)
+      params.split(' | ')
+    end
+  end
+
+  # COMMAND MODE PROMPT (Like vim command or tmux command)
+  # You can add custom prompt & color. And able to use with multiline.
+  # How to use?
+  # {% shell_cmd <prompt_symbol> | <prompt_color:#000000> %}
+  # content...
+  # {% endshell_cmd %}
+  class ShellCmd < Liquid::Block
+    def initialize(tag_name, input, tokens)
+      super
+      @input = input
+    end
+
+    def render(context)
+      params        = split_params(@input)
+      prompt_symbol = params[0]&.strip
+      prompt_color  = params[1]&.strip if params.length > 1
+
+      commands = super.split("\n")
+      output  = '<pre>'
+      output += commands[1..].map do |i|
+        "<span class='cmd' #{"style='color:#{prompt_color};'" unless prompt_color.nil?}>" \
+        "#{prompt_symbol.nil? ? ':' : prompt_symbol}</span><b>#{i.rstrip}</b><br>"
       end.join.to_s
       output += '</pre>'
       output
@@ -58,4 +90,5 @@ end
 
 Liquid::Template.register_tag('shell_root', Jekyll::ShellRoot)
 Liquid::Template.register_tag('shell_user', Jekyll::ShellUser)
-Liquid::Template.register_tag('shell_cmd',  Jekyll::ShellCommand)
+Liquid::Template.register_tag('shell_term', Jekyll::ShellTerm)
+Liquid::Template.register_tag('shell_cmd',  Jekyll::ShellCmd)
